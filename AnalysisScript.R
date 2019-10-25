493745594c41c742da83b03f45000afeb6426c10
julian.romeo.hildebrandt <julian.romeo.hildebrandt@rwth-aachen.de>
2019-10-25 14:42:04 +0200

    AnalysisScript angelegt

---
+++ AnalysisScript.R
@@ -0,0 +1,10 @@
+# AnalysisScript.R
+## Teamnamen: Gelbe Tapete
+
+# Pakete laden: ----
+
+# Rohdaten laden: ----
+
+# DataCleaning und Skalenberechnung: ----
+
+# Statistische Analyse und Grafiken: ----
